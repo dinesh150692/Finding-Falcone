db0b1117dde1c97639f153b0495e93be304d04d0
Dinesh kumar D <dinesh@innoventes.co>
2018-10-06 22:20:48 +0530

    Removed Redux Logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,13 @@
 /* Library Imports */
 import ReduxThunk from 'redux-thunk';
-import logger from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux';
 /* Reducer Imports */
 import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
-	if(process.env.NODE_ENV !== 'production'){
-		return createStore(
-			rootReducer,
-			initialState,
-			applyMiddleware(ReduxThunk, logger)
-		);
-	}else{
 		return createStore(
 			rootReducer,
 			initialState,
 			applyMiddleware(ReduxThunk)
 		);
-	}
 }
